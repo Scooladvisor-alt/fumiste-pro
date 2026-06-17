45bf097e0e9a1aad3b449cccf9f842d146d256ba
Antownin <antownin@gmail.com>
2026-06-17 07:49:06 +0000

    File changes

--- src/components/agenda/AppointmentDialog.jsx
+++ src/components/agenda/AppointmentDialog.jsx
@@ -54,7 +54,8 @@ export default function AppointmentDialog({
       titleEdited: !!base.title,
       notes: base.notes || "",
     });
-  }, [open, appointment, defaultStart, defaultEnd, types, clients]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [open, appointment, defaultStart, defaultEnd]);
 
   if (!form) return null;
 
